ee300343763b287e4c1746ede31cce1727e61bdd
Abeygunasekara D T <140488730+IT22303684@users.noreply.github.com>
2024-07-10 16:31:51 +0530

    first commit

---
+++ models/RItemsModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import {RITEM_CATEGORY , RITEM_STATUS} from "../Utils/constants.js";
+
+const recyclableItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: Object.values(RITEM_CATEGORY),
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    Location : {
+        type: String,
+        required: false,
+    },
+    weight: {
+        type: Number,
+        required: false,
+    },
+    status: {
+        type: String,
+        enum: Object.values(RITEM_STATUS),
+        default: 'Available',
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    isSold: {
+        type: Boolean,
+        default: false,
+    },
+    buyerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: false,
+    },
+    createdBy : {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    itemPhoto: String,
+    itemPhotoPublicId: String,
+} , {timestamps: true});
+
+
+
+export default mongoose.model('RItem', recyclableItemSchema);
