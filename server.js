a950416c022b3b94bbdcd3d70f5bd76fa5ad71ec
Abeygunasekara D T <140488730+IT22303684@users.noreply.github.com>
2024-08-17 22:48:56 +0530

    update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, './Client/dist')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 
 
@@ -75,7 +75,7 @@ app.use('/api/v1/auth', authRouter);
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './Client/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 
