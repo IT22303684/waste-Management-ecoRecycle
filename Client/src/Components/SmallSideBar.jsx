85d30ab93700dbb3a433525a32ec60fd50ae2e0d
shaniranasinghe <it22371768@my.sliit.lk>
2024-11-11 20:59:43 +0530

    First

--- Client/src/Components/SmallSideBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Wrapper from "../assets/wrappers/SmallSidebar";
-
-const SmallSideBar = () => {
-  return <div>SmallsideBar </div>;
-};
-
-export default SmallSideBar;
